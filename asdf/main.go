bb3c8b6757b9184356d5d931057113e89dd87a56
Anders K. Pettersen <staticaland@users.noreply.github.com>
2025-10-27 20:35:19 +0100

    feat: Add asdf base

---
+++ asdf/main.go
@@ -0,0 +1,32 @@
+// A Dagger module for working with asdf version manager
+//
+// This module provides functionality for managing runtime versions using asdf.
+
+package main
+
+import (
+	"fmt"
+
+	"dagger/asdf/internal/dagger"
+)
+
+func New(
+	// renovate: datasource=docker depName=asdfvm/asdf
+	// +default="alpine-v0.17.0@sha256:9744fdf066a668d477186560e2680f87bc935d6f1f17d020c00db83e1006d187"
+	imageTag string,
+) *Asdf {
+	return &Asdf{
+		ImageTag: imageTag,
+	}
+}
+
+type Asdf struct {
+	ImageTag string
+}
+
+// Base returns the base container with asdf installed
+func (m *Asdf) Base() *dagger.Container {
+	return dag.Container().
+		From(fmt.Sprintf("asdfvm/asdf:%s", m.ImageTag)).
+		WithoutEntrypoint()
+}
