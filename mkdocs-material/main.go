03ed260de0ff46ba354ea65e90c9ec91ee428ec7
Anders K. Pettersen <staticaland@users.noreply.github.com>
2025-10-29 21:56:28 +0100

    feat: Add mkdocs-material module
    Add Dagger module for MkDocs Material with base container configuration.

---
+++ mkdocs-material/main.go
@@ -0,0 +1,32 @@
+// A Dagger module for MkDocs Material
+//
+// This module provides functions for working with MkDocs Material,
+// a Material Design theme for MkDocs static site generator.
+
+package main
+
+import (
+	"dagger/mkdocs-material/internal/dagger"
+	"fmt"
+)
+
+func New(
+	// renovate: datasource=docker depName=squidfunk/mkdocs-material
+	// +default="9.6.22@sha256:f5c556a6d30ce0c1c0df10e3c38c79bbcafdaea4b1c1be366809d0d4f6f9d57f"
+	imageTag string,
+) *MkdocsMaterial {
+	return &MkdocsMaterial{
+		ImageTag: imageTag,
+	}
+}
+
+type MkdocsMaterial struct {
+	ImageTag string
+}
+
+// Base returns the base container with MkDocs Material installed
+func (m *MkdocsMaterial) Base() *dagger.Container {
+	return dag.Container().
+		From(fmt.Sprintf("squidfunk/mkdocs-material:%s", m.ImageTag)).
+		WithoutEntrypoint()
+}
