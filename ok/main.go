17b71931e20ec3ef9092ab0a6cb38efd9871b93f
Anders K. Pettersen <staticaland@users.noreply.github.com>
2025-11-04 21:27:15 +0100

    feat: add Boilerplate to ok module

--- ok/main.go
+++ ok/main.go
@@ -8,9 +8,10 @@ import (
 
 type Ok struct{}
 
-// Base returns a container with ok installed via mise
+// Base returns a container with boilerplate and ok installed via mise
 func (m *Ok) Base() *dagger.Container {
 	return dag.Mise().Base().
+		WithExec([]string{"mise", "use", "--global", "ubi:gruntwork-io/boilerplate"}).
 		WithExec([]string{"mise", "use", "--global", "ubi:oslokommune/ok"}).
 		WithEnvVariable("PATH", "/root/.local/share/mise/shims:$PATH", dagger.ContainerWithEnvVariableOpts{
 			Expand: true,
